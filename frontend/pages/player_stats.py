06071aa20784fa22413d89b250d0faeb0c346eea
Zak Nolte <zen1301@gmail.com>
2023-10-08 15:40:31 -0700

    add simple api requests

--- frontend/pages/player_stats.py
+++ frontend/pages/player_stats.py
@@ -1,10 +1,29 @@
 import dash
 from dash import html
+import aiohttp
+import asyncio
 
 dash.register_page(__name__, path="/players")
 
-layout = html.Div(
+async def get_player_stats():
+
+    async with aiohttp.ClientSession() as session:
+
+        api_url = 'http://127.0.0.1:8000/api/players/'
+        async with session.get(api_url) as resp:
+            data = await resp.json()
+    
+    return data
+    
+
+
+
+def layout():
+    players = asyncio.run(get_player_stats())
+    print(players)
+    return html.Div(
     [
         html.H1("player stats go here"),
+        html.Div([html.H1(player['first_name']) for player in players])
     ]
 )
