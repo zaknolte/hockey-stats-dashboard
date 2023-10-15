f3692bf66c1b3087c6d8f8f877cb66032eb8f0a8
Zak Nolte <zen1301@gmail.com>
2023-10-14 17:14:08 -0700

    add full_name

--- backend/playerstats/api.py
+++ backend/playerstats/api.py
@@ -1,5 +1,6 @@
 from ninja import ModelSchema, Router
 from typing import List
+
 from .models import PlayerInfo, PlayerSeason
 
 
@@ -13,6 +14,7 @@ class PlayerInfoSchema(ModelSchema):
             "id",
             "first_name",
             "last_name",
+            "full_name",
             "team_name",
             "picture",
         ]
@@ -22,11 +24,13 @@ class PlayerInfoSchema(ModelSchema):
 def get_players(request):
     return PlayerInfo.objects.all()
 
+
 player_season_router = Router()
 
 
 class PlayerSeasonSchema(ModelSchema):
     player: PlayerInfoSchema
+
     class Config:
         model = PlayerSeason
         model_fields = [
@@ -36,10 +40,39 @@ class PlayerSeasonSchema(ModelSchema):
             "season_type",
             "goals",
             "assists",
-            "points"
+            "points",
         ]
 
 
 @player_season_router.get("/", response=List[PlayerSeasonSchema])
-def get_player_season(request):
-    return PlayerSeason.objects.all().order_by('-season')
+def get_player_season(request, season=2023, season_type="Regular Season", team_name="All Teams"):
+    if team_name == "All Teams":
+        return PlayerSeason.objects.filter(season=season, season_type=season_type)
+    else:
+        return PlayerSeason.objects.filter(
+            season=season, season_type=season_type, player__team_name=team_name
+        )
+
+
+class SeasonListSchema(ModelSchema):
+    class Config:
+        model = PlayerSeason
+        model_fields = [
+            "season"
+        ]
+        model_fields_optional = ['season']
+        
+        
+@player_season_router.get("/current_season", response=SeasonListSchema)
+def get_current_season(request):
+    return PlayerSeason.objects.order_by("-season")[0]
+
+
+@player_season_router.get("/all_seasons", response=List[SeasonListSchema])
+def get_all_seasons(request):
+    return PlayerSeason.objects.values("season").distinct()
+        
+        
+@player_season_router.get("/all_season_types")
+def get_season_types(request):
+    return dict(PlayerSeason.season_type_choices)
