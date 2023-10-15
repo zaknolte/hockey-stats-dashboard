0e59745334fb4a6fe6c2705c9baded54ea94eede
Zak Nolte <zen1301@gmail.com>
2023-10-14 17:12:01 -0700

    use django slugify

--- frontend/helpers.py
+++ frontend/helpers.py
@@ -1,8 +1,2 @@
 def stringify_season(season):
     return f"{season}-{season + 1}"
-
-
-def slugify_item(*args, **kwargs):
-    first = "-".join(str(i).strip(". ") for i in args)
-    second = "-".join(str(kwargs[i]).strip(". ") for i in kwargs)
-    return "-".join([first, second])
