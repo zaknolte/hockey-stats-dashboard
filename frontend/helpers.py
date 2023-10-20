b5e54eef5e325b69f7424c2d569e8214147ee203
Zak Nolte <zen1301@gmail.com>
2023-10-19 18:28:04 -0700

    refactor + add initial ag Grid data

--- frontend/helpers.py
+++ frontend/helpers.py
@@ -1,2 +1,31 @@
 def stringify_season(season):
     return f"{season}-{season + 1}"
+
+
+ag_grid_cols_rename = {
+    "player.full_name": "Name",
+    "player.team_name": "Team",
+    "player.position": "Position",
+    "season": "Season",
+    "season_type": "Season Type",
+}
+
+ag_grid_cols_reorder = [
+    "Name",
+    "Season",
+    "Season Type",
+    "Team",
+    "Position"
+]
+
+ag_grid_cols_exclude = [
+    "player.id",
+    "player.first_name",
+    "player.last_name",
+    "player.picture"
+]
+
+def update_ag_grid_display_cols(df):
+    updated_df = df.rename(columns=ag_grid_cols_rename)
+    column_list = ag_grid_cols_reorder + [i for i in updated_df.columns if i not in ag_grid_cols_exclude and i not in ag_grid_cols_reorder]
+    return updated_df[column_list]
--- frontend/pages/player_season_stats.py
+++ frontend/pages/player_season_stats.py
@@ -1,17 +1,20 @@
 import dash
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
+import dash_ag_grid as dag
+
 import aiohttp
 import asyncio
 import base64
 import pandas as pd
 import numpy as np
+
 from django.utils.text import slugify
 from pathlib import Path
 from io import StringIO
 
 # from app import DJANGO_ROOT
-from helpers import stringify_season
+from helpers import stringify_season, ag_grid_cols_rename, ag_grid_cols_reorder, update_ag_grid_display_cols
 
 dash.register_page(__name__, path="/players")
 
@@ -48,6 +51,7 @@ def format_image(image_url):
 def build_query_url(season=CURRENT_SEASON, season_type="Regular Season", team="All Teams"):
     return f"?season={season}&season_type={season_type}&team_name={team}"
 
+
 def get_player_options(position):  
     player_options = None
     
@@ -66,7 +70,7 @@ def get_player_options(position):
 def filter_data_by_position(df, position):
     filter_list = get_player_options(position)
     # find the set union where player position in contained in filter list
-    return df[df["player.position"].apply(lambda x: bool(set(x) & set(filter_list)))]
+    return df[df[ag_grid_cols_rename["player.position"]].apply(lambda x: bool(set(x) & set(filter_list)))]
 
 
 def query_to_formatted_df(query):
@@ -77,14 +81,15 @@ def query_to_formatted_df(query):
         return vals
     
     df = pd.json_normalize(asyncio.run(query_player_stats(query))).set_index("id")
-    # player.positions queried as list of dicts
-    # reduce player.positions to just a list of dict values
-    df["player.position"] = df["player.position"].apply(split_player_position_col)
+    df = df.rename(columns=ag_grid_cols_rename)
+    # Positions queried as list of dicts
+    # reduce Positions to just a list of dict values
+    df[ag_grid_cols_rename["player.position"]] = df[ag_grid_cols_rename["player.position"]].apply(split_player_position_col)
     return df
     
 
 def get_all_teams(df, add_all=True):
-    teams_list = pd.unique(df["player.team_name"])
+    teams_list = pd.unique(df[ag_grid_cols_rename["player.team_name"]])
     teams_list.sort()
     if add_all:
         teams_list = np.insert(teams_list, 0, "All Teams")
@@ -128,6 +133,57 @@ def get_filter_dropdowns_layout(df):
             "paddingTop": 50,
         },
     )
+    
+
+def get_player_position_groups_layout():
+    return dbc.RadioItems(
+        [
+            {"label": "All Skaters", "value": "All Skaters"},
+            {"label": "Forwards", "value": "Forwards"},
+            {"label": "Defense", "value": "Defense"},
+            {"label": "Goalies", "value": "Goalies"},
+        ],
+        style={
+            "display": "flex",
+            "justifyContent": "center",
+            "paddingTop": 25,
+        },
+        id="player-position-groups",
+        input_class_name="btn-check",
+        label_class_name="btn btn-outline-primary",
+        value="All Skaters"
+    )
+    
+
+def get_player_position_options_layout():
+    return dbc.RadioItems(
+        [
+            {"label": "All Positions", "value": "All Positions"},
+            {"label": "C", "value": "C"},
+            {"label": "RW", "value": "RW"},
+            {"label": "LW", "value": "LW"},
+        ],
+        style={
+            "display": "flex",
+            "justifyContent": "center",
+            "paddingTop": 15,
+            "paddingBottom": 15,
+        },
+        id="player-position-options",
+        input_class_name="btn-check",
+        label_class_name="btn btn-outline-primary",
+        value="All Positions"
+    )
+
+
+def get_agGrid_layout(df):
+    columnDefs = [{"field": i} for i in update_ag_grid_display_cols(df).columns]
+    return dag.AgGrid(
+        rowData=df.to_dict("records"),
+        columnDefs=columnDefs,
+        id="player-stats-grid",
+        style={"paddingLeft": 50, "paddingRight": 50}
+    )
 
 
 def get_league_leaders_layout(df, stats_list):
@@ -137,7 +193,7 @@ def get_league_leaders_layout(df, stats_list):
             get_leaders_layout(df, stats_list[1], dropdown_id=2),
             get_leaders_layout(df, stats_list[2], dropdown_id=3),
         ],
-        style={"display": "flex", "justifyContent": "space-around"},
+        style={"display": "flex", "justifyContent": "space-around", "minHeight": 350},
     )
     return layouts
 
@@ -175,8 +231,8 @@ def get_leaders_layout_rows(df, stat, position):
             [
                 dbc.Col(
                     dcc.Link(
-                        value["player.full_name"],
-                        href=f'player/{slugify(value["player.full_name"])}',
+                        value[ag_grid_cols_rename["player.full_name"]],
+                        href=f'player/{slugify(value[ag_grid_cols_rename["player.full_name"]])}',
                     ),
                     width=8,
                 ),
@@ -185,13 +241,14 @@ def get_leaders_layout_rows(df, stat, position):
                 ),
             ]
         )
-        for row, value in leaders[["player.full_name", stat]].iterrows()
+        for row, value in leaders[[ag_grid_cols_rename["player.full_name"], stat]].iterrows()
     ]
 
 
 def layout():
     # get database data with defaults for current regular season for all teams
     players_df = query_to_formatted_df(build_query_url())
+    
     return html.Div(
         [
             get_filter_dropdowns_layout(players_df),
@@ -205,54 +262,59 @@ def layout():
                             "paddingTop": 50,
                         },
                     ),
-                    dbc.RadioItems(
-                        [
-                            {"label": "All Skaters", "value": "All Skaters"},
-                            {"label": "Forwards", "value": "Forwards"},
-                            {"label": "Defense", "value": "Defense"},
-                            {"label": "Goalies", "value": "Goalies"},
-                        ],
-                        style={
-                            "display": "flex",
-                            "justifyContent": "center",
-                            "paddingTop": 25,
-                        },
-                        id="player-position-groups",
-                        input_class_name="btn-check",
-                        label_class_name="btn btn-outline-primary",
-                        value="All Skaters"
-                    ),
-                    dbc.RadioItems(
-                        [
-                            {"label": "All Positions", "value": "All Positions"},
-                            {"label": "C", "value": "C"},
-                            {"label": "RW", "value": "RW"},
-                            {"label": "LW", "value": "LW"},
-                        ],
-                        style={
-                            "display": "flex",
-                            "justifyContent": "center",
-                            "paddingTop": 15,
-                            "paddingBottom": 15,
-                        },
-                        id="player-position-options",
-                        input_class_name="btn-check",
-                        label_class_name="btn btn-outline-primary",
-                        value="All Positions"
-                    ),
+                    get_player_position_groups_layout(),
+                    get_player_position_options_layout(),
                 ]
             ),
             get_league_leaders_layout(players_df, ["goals", "assists", "points"]),
-            # add dash aggrid
+            get_agGrid_layout(players_df),
             dcc.Store(data=players_df.to_json(), id="season-stats-df"),
         ]
     )
 
 
 @callback(
+    Output("season-stats-df", "data"),
+    Input("dropdown-season", "value"),
+    Input("dropdown-season-type", "value"),
+    Input("dropdown-team", "value"),
+    Input("player-position-options", "value"),
+    State("player-position-groups", "value"),
+    prevent_initial_call=True
+)
+def update_displayed_data(season, season_type, team, position, position_group):
+    formatted_season = int(season[:4])
+    df = query_to_formatted_df(build_query_url(formatted_season, season_type, team))
+    
+    df = filter_data_by_position(df, position_group)
+    
+    if position != "All Positions":
+        # players may be assigned more than one position
+        # create bool mask to determine if selected position matches any of the player positions
+        mask = df[ag_grid_cols_rename["player.position"]].apply(lambda x: position in x)
+        
+        df = df[mask]
+    
+    return df.to_json()
+
+
+@callback(
+    Output("player-position-options", "options"),
+    Output("player-position-options", "value"),
+    Input("player-position-groups", "value"),
+    prevent_initial_call=True
+)
+def update_player_position_options(player_group):
+    options =  ["All Positions"] + get_player_options(player_group)
+    value = options[0]
+    return options, value
+
+
+@callback(
     Output("rows-leader-stat-1", "children"),
     Output("rows-leader-stat-2", "children"),
     Output("rows-leader-stat-3", "children"),
+    Output("player-stats-grid", "rowData"),
     Input("dropdown-leader-stat-1", "value"),
     Input("dropdown-leader-stat-2", "value"),
     Input("dropdown-leader-stat-3", "value"),
@@ -267,39 +329,5 @@ def update_leader_stats(stat_left, stat_center, stat_right, player_position, dat
     center = get_leaders_layout_rows(df, stat_center.lower(), player_position)
     right = get_leaders_layout_rows(df, stat_right.lower(), player_position)
     
-    return left, center, right
-
-
-@callback(
-    Output("season-stats-df", "data"),
-    Input("dropdown-season", "value"),
-    Input("dropdown-season-type", "value"),
-    Input("dropdown-team", "value"),
-    Input("player-position-groups", "value"),
-    Input("player-position-options", "value"),
-    prevent_initial_call=True
-)
-def update_displayed_data(season, season_type, team, position_group, position):
-    formatted_season = int(season[:4])
-    df = query_to_formatted_df(build_query_url(formatted_season, season_type, team))
-    
-    df = filter_data_by_position(df, position_group)
-    
-    if position != "All Positions":
-        # players may be assigned more than one position
-        # create bool mask to determine if selected position matches any of the player positions
-        mask = df["player.position"].apply(lambda x: position in x)
-        
-        df = df[mask]
-    
-    return df.to_json()
-
-@callback(
-    Output("player-position-options", "options"),
-    Output("player-position-options", "value"),
-    Input("player-position-groups", "value")
-)
-def update_player_position_options(player_group):
-    options =  ["All Positions"] + get_player_options(player_group)
-    value = options[0]
-    return options, value
+    df = update_ag_grid_display_cols(df)
+    return left, center, right, df.to_dict("records")
