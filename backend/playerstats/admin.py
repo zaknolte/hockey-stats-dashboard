5b98d3a8e543d2abca580422844f4f4d3f16d410
Zak Nolte <zen1301@gmail.com>
2023-10-27 16:24:42 -0700

    team display fix

--- backend/playerstats/admin.py
+++ backend/playerstats/admin.py
@@ -5,11 +5,15 @@ from .models import Player, PlayerPosition
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "team_name")
-    search_fields = ["last_name", "team_name"]
+    list_display = ("first_name", "last_name", "get_team_name")
+    search_fields = ["last_name", "team__name"]
 
     def get_ordering(self, request):
         return [Lower("last_name")]  # sort case insensitive
+    
+    @admin.display(ordering='team__name', description='Team')
+    def get_team_name(self, obj):
+        return obj.team.name
 
 
 admin.site.register(Player, PlayerAdmin)
