7a9ac5947f4379b92353941f8f6839337258e0e4
zaknolte <noltezak@gmail.com>
2023-12-04 21:52:28 -0700

    remove old database file

--- delete_migrations.py
+++ delete_migrations.py
@@ -4,13 +4,17 @@ def del_migrations():
 
     print("Deleting migrations...")
     for root_dir, dirs, files in os.walk("./backend"):
+        for file in files:
+            if file.endswith(".sqlite3"):
+                print(f"Removing: {file}")
+                os.remove(f"{root_dir}/{file}")
         for dir in dirs:
             for sub_root, sub_dir, sub_file in os.walk(f"{root_dir}/{dir}"):
                 for s in sub_dir:
                     if s == "migrations":
                         for i, j, f in os.walk(f"{root_dir}/{dir}/{s}", topdown=False):
                             for m in f:
-                                if "000" in m and m.endswith(".py"):
+                                if "_initial" in m and m.endswith(".py"):
                                     print(f"Removing: {dir} - {m}")
                                     os.remove(f"{root_dir}/{dir}/{s}/{m}")
     print("All migrations deleted!")
